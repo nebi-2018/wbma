d6aaee771e92827abaf19f2528553a27e7b35ef6
nebi <snoopafr@gmail.com>
2020-01-18 18:20:00 +0200

    The hooks part has been done!

--- components/List.js
+++ components/List.js
@@ -3,12 +3,20 @@ import React, { useContext } from "react";
 import { FlatList } from "react-native";
 import ListItem from "./ListItem";
 import { MediaContext } from "../contexts/MediaContext";
+import { useFetch } from "../hooks/APIHooks";
 
 const List = () => {
   const [media, setMedia] = useContext(MediaContext);
+  const [data, loading] = useFetch(
+    "https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json"
+  );
+  console.log(data);
+  setMedia(data);
+
   return (
     <FlatList
       data={media}
+      keyExtractor={(item, index) => index.toString()}
       renderItem={({ item }) => <ListItem singleMedia={item} />}
     />
   );
